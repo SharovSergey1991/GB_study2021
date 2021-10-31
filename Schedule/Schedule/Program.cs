f766ce8ee7ad7b6a34b97f14d8cfbd0231757db6
Sharov Sergey <sharovsergey1991@gmail.com>
2021-10-31 10:49:52 +1000

    Домашнее задание №2 пункт 6

---
+++ Schedule/Schedule/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Schedule
+{
+    class Program
+    {
+        [Flags]
+        public enum Days
+        {
+            Понедельник    = 0b_0000001,
+            Вторник        = 0b_0000010,
+            Среда          = 0b_0000100,
+            Четверг        = 0b_0001000,
+            Пятница        = 0b_0010000,
+            Суббота        = 0b_0100000,
+            Воскресенье    = 0b_1000000
+        }
+
+        static void Main(string[] args)
+        {
+            Days scheduleOffice1 = Days.Понедельник | Days.Вторник | Days.Среда | Days.Четверг | Days.Пятница | Days.Суббота;
+            Days scheduleOffice2 = Days.Вторник | Days.Среда | Days.Четверг | Days.Пятница | Days.Воскресенье;
+            Days scheduleOffice3 = Days.Понедельник | Days.Среда | Days.Четверг | Days.Суббота | Days.Воскресенье;
+
+            Console.WriteLine($"Рабочие дни центрального офиса: {scheduleOffice1}.");
+            Console.WriteLine($"Рабочие дни дополнительного офиса на Енисейской: {scheduleOffice2}.");
+            Console.WriteLine($"Рабочие дни дополнительного офиса на Астафьева: {scheduleOffice3}.");
+            Console.ReadLine();
+        }
+    }
+}
