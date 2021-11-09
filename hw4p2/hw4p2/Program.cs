6b81f9df7578f91703cdf3feecb6ca52c27fec19
Sharov Sergey <sharovsergey1991@gmail.com>
2021-11-09 21:46:13 +1000

    Домашнее задание №4 пункт 2

---
+++ hw4p2/hw4p2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hw4p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void Sum(string numbers)
+            {
+                var arrayNumbers = numbers.Split(' ');
+                int sum = 0;
+                for (int i = 0; i < arrayNumbers.Length; i++)
+                {
+                    sum += Int32.Parse(arrayNumbers[i]);
+                }
+                Console.WriteLine($"Сумма введённых чисел: {sum}");
+            }
+
+            Console.Write("Введите числа через пробел: ");
+            string numbers = Console.ReadLine();
+
+            Sum(numbers);
+
+            Console.ReadLine();
+        }
+    }
+}
