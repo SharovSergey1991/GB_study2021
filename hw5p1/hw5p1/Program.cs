0a4c9bfb0417f99e663488969bd4f49421089c70
Sharov Sergey <sharovsergey1991@gmail.com>
2021-11-14 20:43:29 +1000

    Домашнее задание 5, пункт 1

---
+++ hw5p1/hw5p1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace hw5p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите произвольный текст");
+            string text = Console.ReadLine();
+            File.WriteAllText("text.txt", text);
+        }
+    }
+}
