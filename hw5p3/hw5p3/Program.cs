d97af687a3a89db62337f280cb284b39815af74e
Sharov Sergey <sharovsergey1991@gmail.com>
2021-11-14 20:45:51 +1000

    Домашнее задание 5, пункт 3

---
+++ hw5p3/hw5p3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace hw5p3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите через пробел любые числа в диапазоне от 0 до 255: ");
+
+            string numbers = Console.ReadLine();
+            string[] arrNumbers = numbers.Split();
+            var arrBytes = new byte[arrNumbers.Length];
+
+            for (int i = 0; i < arrNumbers.Length; i++)
+            {
+                arrBytes[i] = Convert.ToByte(arrNumbers[i]);
+            }
+
+            File.WriteAllBytes("binary.bin", arrBytes);
+        }
+    }
+}
