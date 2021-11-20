de9a3feb162ee62bbac149b4794ca07023ef8731
Sharov Sergey <sharovsergey1991@gmail.com>
2021-11-21 09:56:36 +1000

    Домашнее задание 7 (приложение для полледующей декомпиляции в dotPeek)

---
+++ hw7/hw7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hw7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вычисление квадратного корня числа");
+            Console.Write("Введите число: ");
+            double num = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"Квадратный корень числа {num} равен {Math.Sqrt(num)}");
+
+            Console.ReadLine();
+        }
+    }
+}
