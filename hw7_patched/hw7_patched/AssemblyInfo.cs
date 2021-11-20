f85395e504fed14d51d65d6b84a29d934f09a6b1
Sharov Sergey <sharovsergey1991@gmail.com>
2021-11-21 09:58:58 +1000

    Домашнее задание 7 (приложение после декомпиляции в dotPeek и с внесенными изменения в MS VS)

---
+++ hw7_patched/hw7_patched/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("hw7")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("hw7")]
+[assembly: AssemblyTitle("hw7")]
+[assembly: AssemblyVersion("1.0.0.0")]
